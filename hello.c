691d1f0d7944fbf07f544852af254252485bbd4c
Emir Ferzan <eferzan2@huskers.unl.edu>
2022-08-24 23:53:13 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World program.
+ */
+
+int main(int argc, char **argv) {
+
+    printf("Hello! I am Emir Ferzan./n");
+    printf("Major is Computer Science!/n");
+
+    return 0;
+}
