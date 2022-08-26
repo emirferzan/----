0ec83de47129dd9326adee84aeaab806234a7a3e
Connor McNeel <cmcneel2@huskers.unl.edu>
2022-08-26 21:17:23 +0000

    added my name and major to the output

--- hello.c
+++ hello.c
@@ -4,9 +4,10 @@
 
 int main(int argc, char **argv) {
 
-    printf("Hello! I am Emir Ferzan.\n");   
+    printf("Hello! I am Emir Ferzan.\n");
     printf("Major is Computer Science!\n");
     printf("I have a minor in math\n");
-
+    printf("I am Connor McNeel\n");
+    printf("I am a Computer Science Major\n");
     return 0;
 }
