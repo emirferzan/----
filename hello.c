682104520fa615a47e7327bfabf42db4cb25cbb1
Emir Ferzan <eferzan2@huskers.unl.edu>
2022-08-26 20:49:59 +0000

    added my info

--- hello.c
+++ hello.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char **argv) {
 
-    printf("Hello! I am Emir Ferzan.\n");
+    printf("Hello! I am Emir Ferzan.\n");   
     printf("Major is Computer Science!\n");
     printf("I have a minor in math\n");
 
